d290e2f7309f52243f0071d3d1f05e3ba87d12e0
Unknown <1079395402@qq.com>
2019-09-26 17:19:36 +0800

    支持SAP hana数据库读操作

---
+++ hanareader/src/main/java/hanareader/Constant.java
@@ -0,0 +1,10 @@
+package hanareader;
+
+/**
+ * @Author liuwenyu
+ * @Description //TODO
+ * @Date 14:37 2019-06-26
+ **/
+public class Constant {
+    static final int DEFAULT_FETCH_SIZE = 1024;
+}
