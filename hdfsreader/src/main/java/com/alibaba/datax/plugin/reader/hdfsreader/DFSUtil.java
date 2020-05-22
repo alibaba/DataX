c9b71dea67643b0ca441ce51be27803cf455f13d
felix.Wang <felix@thinkingdata.cn>
2020-05-22 20:15:34 +0800

    修复文件过大，如果只读取第一个分片，会遗漏读取的数据。

--- hdfsreader/src/main/java/com/alibaba/datax/plugin/reader/hdfsreader/DFSUtil.java
+++ hdfsreader/src/main/java/com/alibaba/datax/plugin/reader/hdfsreader/DFSUtil.java
@@ -332,25 +332,30 @@ public class DFSUtil {
                 //Each file as a split
                 //TODO multy threads
                 InputSplit[] splits = in.getSplits(conf, 1);
-
-                RecordReader reader = in.getRecordReader(splits[0], conf, Reporter.NULL);
-                Object key = reader.createKey();
-                Object value = reader.createValue();
-                // 获取列信息
-                List<? extends StructField> fields = inspector.getAllStructFieldRefs();
-
-                List<Object> recordFields;
-                while (reader.next(key, value)) {
-                    recordFields = new ArrayList<Object>();
-
-                    for (int i = 0; i <= columnIndexMax; i++) {
-                        Object field = inspector.getStructFieldData(value, fields.get(i));
-                        recordFields.add(field);
-                    }
-                    transportOneRecord(column, recordFields, recordSender,
-                            taskPluginCollector, isReadAllColumns, nullFormat);
+                if(splits.length==0){
+                    LOG.warn("======== 数据文件分片为空跳出读取！==========");
+                    return ;
                 }
-                reader.close();
+                for(InputSplit split:splits){
+                    RecordReader reader = in.getRecordReader(split, conf, Reporter.NULL);
+                    Object key = reader.createKey();
+                    Object value = reader.createValue();
+                    // 获取列信息
+                    List<? extends StructField> fields = inspector.getAllStructFieldRefs();
+
+                    List<Object> recordFields;
+                    while (reader.next(key, value)) {
+                        recordFields = new ArrayList<Object>();
+
+                        for (int i = 0; i <= columnIndexMax; i++) {
+                            Object field = inspector.getStructFieldData(value, fields.get(i));
+                            recordFields.add(field);
+                        }
+                        transportOneRecord(column, recordFields, recordSender,
+                                taskPluginCollector, isReadAllColumns, nullFormat);
+                    }
+                    reader.close();
+                }
             } catch (Exception e) {
                 String message = String.format("从orcfile文件路径[%s]中读取数据发生异常，请联系系统管理员。"
                         , sourceOrcFilePath);
