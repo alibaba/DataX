24ad8c86d9f623657b47ab1528283d1689d1628f
wangweiwei <wangweiwei>
2021-01-20 15:48:47 +0800

    read code

--- plugin-rdbms-util/src/main/java/com/alibaba/datax/plugin/rdbms/writer/Constant.java
+++ plugin-rdbms-util/src/main/java/com/alibaba/datax/plugin/rdbms/writer/Constant.java
@@ -4,6 +4,7 @@ package com.alibaba.datax.plugin.rdbms.writer;
  * 用于插件解析用户配置时，需要进行标识（MARK）的常量的声明.
  */
 public final class Constant {
+
     public static final int DEFAULT_BATCH_SIZE = 2048;
 
     public static final int DEFAULT_BATCH_BYTE_SIZE = 32 * 1024 * 1024;
