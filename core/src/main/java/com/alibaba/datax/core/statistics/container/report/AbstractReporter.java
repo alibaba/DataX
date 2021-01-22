373227d91ca02b66e68e9e4edc2cb3c579aa46a1
wangweiwei <wangweiwei>
2021-01-22 14:53:20 +0800

    read AbstractReporter's code on  0122

--- core/src/main/java/com/alibaba/datax/core/statistics/container/report/AbstractReporter.java
+++ core/src/main/java/com/alibaba/datax/core/statistics/container/report/AbstractReporter.java
@@ -7,8 +7,20 @@ import com.alibaba.datax.core.statistics.communication.Communication;
  */
 public abstract class AbstractReporter {
 
+  /**
+   * 汇报job的communication信息给上级（目前datax中job无上级，所以子类中do nothing）
+   *
+   * @param jobId         Long
+   * @param communication Communication
+   */
   public abstract void reportJobCommunication(Long jobId, Communication communication);
 
+  /**
+   * 汇报TG的communication信息给上级
+   *
+   * @param taskGroupId   Integer
+   * @param communication Communication
+   */
   public abstract void reportTGCommunication(Integer taskGroupId, Communication communication);
 
 }
