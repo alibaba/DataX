cdb884d0b779e9ac99229643add53060549878d1
wangweiwei <wangweiwei>
2021-01-24 16:14:54 +0800

    read transformer's code on 0124

--- core/src/main/java/com/alibaba/datax/core/transport/transformer/GroovyTransformer.java
+++ core/src/main/java/com/alibaba/datax/core/transport/transformer/GroovyTransformer.java
@@ -13,7 +13,7 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * no comments.
+ * Groovy类的transformer
  * Created by liqiang on 16/3/4.
  */
 public class GroovyTransformer extends Transformer {
@@ -24,9 +24,26 @@ public class GroovyTransformer extends Transformer {
 
   private Transformer groovyTransformer;
 
+  /**
+   * 参数 <br>
+   * 第一个参数： groovy code <br>
+   * 第二个参数（列表或者为空）：extraPackage <br>
+   * 备注： <br>
+   * dx_groovy只能调用一次。不能多次调用。 <br>
+   * groovy code中支持java.lang, java.util的包，可直接引用的对象有record，以及element下的各种 <br>
+   * column（BoolColumn.class,BytesColumn.class,DateColumn.class,DoubleColumn.class,LongColumn.class,
+   * <br>
+   * StringColumn.class）。不支持其他包，如果用户有需要用到其他包，可设置extraPackage，注意extraPackage不支持第三方jar包。 <br>
+   * groovy code中，返回更新过的Record（比如record.setColumn(columnIndex, new StringColumn(newValue));）， <br>
+   * 或者null。返回null表示过滤此行。 <br>
+   * 用户可以直接调用静态的Util方式（GroovyTransformerStaticUtil），目前GroovyTransformerStaticUtil的方法列表 (按需补充)： <br>
+   *
+   * @param record Record 行记录，UDF进行record的处理后，更新相应的record
+   * @param paras  Object transformer函数参数
+   * @return Record
+   */
   @Override
   public Record evaluate(Record record, Object... paras) {
-
     if (groovyTransformer == null) {
       //全局唯一
       if (paras.length < 1 || paras.length > 2) {
@@ -46,21 +63,23 @@ public class GroovyTransformer extends Transformer {
   }
 
   /**
-   * 初始化 GroovyTransformer
-   * @param code String
-   * @param extraPackage List<String>
+   * 初始化 GroovyTransformer。<br>
+   * 1 根据code和包列表，构造出完整的groovy代码段。<br>
+   * 2 反射加载该groovy。<br>
+   * 3 将2反射构造出的groovy对象强制类型转为，最后赋给groovyTransformer（Transformer类型）。<br>
+   *
+   * @param code         String  Groovy代码片段
+   * @param extraPackage List<String> 额外的import的包
    */
   private void initGroovyTransformer(String code, List<String> extraPackage) {
     GroovyClassLoader loader = new GroovyClassLoader(GroovyTransformer.class.getClassLoader());
     String groovyRule = getGroovyRule(code, extraPackage);
-
     Class groovyClass;
     try {
       groovyClass = loader.parseClass(groovyRule);
     } catch (CompilationFailedException cfe) {
       throw DataXException.asDataXException(TRANSFORMER_GROOVY_INIT_EXCEPTION, cfe);
     }
-
     try {
       Object t = groovyClass.newInstance();
       if (!(t instanceof Transformer)) {
@@ -74,7 +93,14 @@ public class GroovyTransformer extends Transformer {
   }
 
 
-  private String getGroovyRule(String expression, List<String> extraPackagesStrList) {
+  /**
+   * 根据code 和 引用的包，构建出groovy代码片段
+   *
+   * @param code
+   * @param extraPackagesStrList
+   * @return
+   */
+  private String getGroovyRule(String code, List<String> extraPackagesStrList) {
     StringBuffer sb = new StringBuffer();
     if (extraPackagesStrList != null) {
       for (String extraPackagesStr : extraPackagesStrList) {
@@ -91,11 +117,8 @@ public class GroovyTransformer extends Transformer {
     sb.append("import java.util.*;");
     sb.append("public class RULE extends Transformer").append("{");
     sb.append("public Record evaluate(Record record, Object... paras) {");
-    sb.append(expression);
+    sb.append(code);
     sb.append("}}");
-
     return sb.toString();
   }
-
-
 }
--- core/src/main/java/com/alibaba/datax/core/transport/transformer/ReplaceTransformer.java
+++ core/src/main/java/com/alibaba/datax/core/transport/transformer/ReplaceTransformer.java
@@ -13,54 +13,68 @@ import java.util.Arrays;
  * Created by liqiang on 16/3/4.
  */
 public class ReplaceTransformer extends Transformer {
-    public ReplaceTransformer() {
-        setTransformerName("dx_replace");
+
+  public ReplaceTransformer() {
+    setTransformerName("dx_replace");
+  }
+
+  /**
+   * 参数：4个 <br>
+   * 第一个参数：字段编号，对应record中第几个字段。 <br>
+   * 第二个参数：字段值的开始位置。 <br>
+   * 第三个参数：需要替换的字段长度。 <br>
+   * 第四个参数：需要替换的字符串。 <br>
+   * 举例： <br>
+   * dx_replace(1,"2","4","****")  column 1的value为“dataxTest”=>"da****est" <br>
+   * dx_replace(1,"5","10","****")  column 1的value为“dataxTest”=>"data****" <br>
+   *
+   * @param record Record 行记录，UDF进行record的处理后，更新相应的record <br>
+   * @param paras  Object transformer函数参数 <br>
+   * @return 从字符串的指定位置（包含）替换指定长度的字符串。如果开始位置非法抛出异常。如果字段为空值， <br>
+   * 直接返回（即不参与本transformer） <br>
+   */
+  @Override
+  public Record evaluate(Record record, Object... paras) {
+
+    int columnIndex;
+    int startIndex;
+    int targetLen;
+    String replaceStr;
+    try {
+      if (paras.length != 4) {
+        throw new RuntimeException("dx_replace paras must be 4");
+      }
+
+      columnIndex = (Integer) paras[0];
+      startIndex = Integer.valueOf((String) paras[1]);
+      targetLen = Integer.valueOf((String) paras[2]);
+      replaceStr = (String) paras[3];
+    } catch (Exception e) {
+      throw DataXException.asDataXException(TransformerErrorCode.TRANSFORMER_ILLEGAL_PARAMETER,
+          "paras:" + Arrays.asList(paras).toString() + " => " + e.getMessage());
     }
-
-    @Override
-    public Record evaluate(Record record, Object... paras) {
-
-        int columnIndex;
-        int startIndex;
-        int length;
-        String replaceString;
-        try {
-            if (paras.length != 4) {
-                throw new RuntimeException("dx_replace paras must be 4");
-            }
-
-            columnIndex = (Integer) paras[0];
-            startIndex = Integer.valueOf((String) paras[1]);
-            length = Integer.valueOf((String) paras[2]);
-            replaceString = (String) paras[3];
-        } catch (Exception e) {
-            throw DataXException.asDataXException(TransformerErrorCode.TRANSFORMER_ILLEGAL_PARAMETER, "paras:" + Arrays.asList(paras).toString() + " => " + e.getMessage());
-        }
-
-        Column column = record.getColumn(columnIndex);
-
-        try {
-            String oriValue = column.asString();
-
-            //如果字段为空，跳过replace处理
-            if(oriValue == null){
-                return  record;
-            }
-            String newValue;
-            if (startIndex > oriValue.length()) {
-                throw new RuntimeException(String.format("dx_replace startIndex(%s) out of range(%s)", startIndex, oriValue.length()));
-            }
-            if (startIndex + length >= oriValue.length()) {
-                newValue = oriValue.substring(0, startIndex) + replaceString;
-            } else {
-                newValue = oriValue.substring(0, startIndex) + replaceString + oriValue.substring(startIndex + length, oriValue.length());
-            }
-
-            record.setColumn(columnIndex, new StringColumn(newValue));
-
-        } catch (Exception e) {
-            throw DataXException.asDataXException(TransformerErrorCode.TRANSFORMER_RUN_EXCEPTION, e.getMessage(),e);
-        }
+    Column column = record.getColumn(columnIndex);
+    try {
+      String oriValue = column.asString();
+      //如果字段为空，跳过replace处理
+      if (oriValue == null) {
         return record;
+      }
+      String newValue;
+      int oriLen = oriValue.length();
+      if (startIndex > oriLen) {
+        throw new RuntimeException(String
+            .format("dx_replace startIndex(%s) out of range(%s)", startIndex, oriLen));
+      }
+      newValue = oriValue.substring(0, startIndex) + replaceStr;
+      if (startIndex + targetLen < oriLen) {
+        newValue = newValue + oriValue.substring(startIndex + targetLen);
+      }
+      record.setColumn(columnIndex, new StringColumn(newValue));
+    } catch (Exception e) {
+      throw DataXException
+          .asDataXException(TransformerErrorCode.TRANSFORMER_RUN_EXCEPTION, e.getMessage(), e);
     }
+    return record;
+  }
 }
