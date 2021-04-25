18f9099142d1305964775efa0ddc656512bffcd7
Locki <love547@sina.com>
2021-04-25 12:45:43 +0800

    增加神通数据库oscarreader插件

---
+++ oscarreader/src/main/java/com/alibaba/datax/plugin/reader/oscarreader/Constant.java
@@ -0,0 +1,11 @@
+package com.alibaba.datax.plugin.reader.oscarreader;
+
+/**
+ * 常量类
+ *
+ * @author LockiJiang
+ * @date 2021/4/25
+ */
+public class Constant {
+    public static final int DEFAULT_FETCH_SIZE = 1024;
+}
