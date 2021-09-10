629fa484120574dc6521e288c37826d0cf7f2c49
fariel <hf@dorisdb.com>
2021-09-10 15:18:38 +0800

    build starrockswriter.tar.gz

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ mvn -U -pl starrockswriter -am clean package assembly:assembly -Dmaven.test.skip
 rm -f starrockswriter.tar.gz
 cd target/datax/datax/plugin/writer/
 tar -czvf starrockswriter.tar.gz starrockswriter
+mv starrockswriter.tar.gz $ShellDir
