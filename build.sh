b725a83ac13c17e082726267b0cc068761cd55da
fariel <hf@dorisdb.com>
2021-09-10 15:10:13 +0800

    bumping version to 1.1.0

--- build.sh
+++ build.sh
@@ -1,1 +1,5 @@
+ShellDir="$( cd "$( dirname "$0"  )" && pwd  )"
+cd $ShellDir
 mvn -U -pl starrockswriter -am clean package assembly:assembly -Dmaven.test.skip=true
+rm -f starrockswriter.tar.gz
+tar -czvf starrockswriter.tar.gz target/datax/datax/plugin/writer/starrockswriter
