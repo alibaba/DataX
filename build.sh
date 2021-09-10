014b240a1ff9710d018b549fca30c49907defe05
fariel <hf@dorisdb.com>
2021-09-10 15:16:51 +0800

    optimize build script

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@ ShellDir="$( cd "$( dirname "$0"  )" && pwd  )"
 cd $ShellDir
 mvn -U -pl starrockswriter -am clean package assembly:assembly -Dmaven.test.skip=true
 rm -f starrockswriter.tar.gz
-tar -czvf starrockswriter.tar.gz target/datax/datax/plugin/writer/starrockswriter
+cd target/datax/datax/plugin/writer/
+tar -czvf starrockswriter.tar.gz starrockswriter
