5dd1984e4b25bb8ddd5d670587f2ed09825e7b90
removedporn <86824510+removedporn@users.noreply.github.com>
2021-09-20 00:03:50 +0800

    Delete common directory

--- common/src/main/java/com/alibaba/datax/common/constant/CommonConstant.java
+++
@@ -1,9 +0,0 @@
-package com.alibaba.datax.common.constant;
-
-public final class CommonConstant {
-    /**
-     * 用于插件对自身 split 的每个 task 标识其使用的资源，以告知core 对 reader/writer split 之后的 task 进行拼接时需要根据资源标签进行更有意义的 shuffle 操作
-     */
-    public static String LOAD_BALANCE_RESOURCE_MARK = "loadBalanceResourceMark";
-
-}
--- common/src/main/java/com/alibaba/datax/common/plugin/PluginCollector.java
+++
@@ -1,9 +0,0 @@
-package com.alibaba.datax.common.plugin;
-
-
-/**
- * 这里只是一个标示类
- * */
-public interface PluginCollector {
-
-}
