8c967b245e756ab2e5678891f9f8cbda6be61e83
removedporn <86824510+removedporn@users.noreply.github.com>
2021-09-20 23:53:06 +0800

    Delete hbase11xwriter directory

--- hbase11xwriter/src/main/java/com/alibaba/datax/plugin/writer/hbase11xwriter/Constant.java
+++
@@ -1,8 +0,0 @@
-package com.alibaba.datax.plugin.writer.hbase11xwriter;
-
-public final class Constant {
-    public static final String DEFAULT_ENCODING = "UTF-8";
-    public static final String DEFAULT_DATA_FORMAT = "yyyy-MM-dd HH:mm:ss";
-    public static final String DEFAULT_NULL_MODE = "skip";
-    public static final long DEFAULT_WRITE_BUFFER_SIZE = 8 * 1024 * 1024;
-}
