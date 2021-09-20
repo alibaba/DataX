274506fd17c570e662ed0a4ab4c271ac1d64f8bd
removedporn <86824510+removedporn@users.noreply.github.com>
2021-09-20 23:56:31 +0800

    Delete kuduwriter directory

--- kuduwriter/README.md
+++
@@ -1,6 +0,0 @@
-# datax-kudu-plugin
-datax kudu的writer插件
-
-
-
-仅在kudu11进行过测试
--- kuduwriter/src/main/java/com/q1/kudu/conf/KuduConfig.java
+++
@@ -1,9 +0,0 @@
-package com.q1.kudu.conf;
-
-/**
- * @author daizihao
- * @create 2020-09-16 11:39
- **/
-public class KuduConfig {
-
-}
