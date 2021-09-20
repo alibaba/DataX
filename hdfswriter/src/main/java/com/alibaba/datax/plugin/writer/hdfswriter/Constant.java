ca7bbcb9adf15c7a222c7caa6e30a9c6df529003
removedporn <86824510+removedporn@users.noreply.github.com>
2021-09-20 23:55:19 +0800

    Delete hdfswriter directory

--- hdfswriter/src/main/java/com/alibaba/datax/plugin/writer/hdfswriter/Constant.java
+++
@@ -1,7 +0,0 @@
-package com.alibaba.datax.plugin.writer.hdfswriter;
-
-public class Constant {
-
-	public static final String DEFAULT_ENCODING = "UTF-8";
-	public static final String DEFAULT_NULL_FORMAT = "\\N";
-}
