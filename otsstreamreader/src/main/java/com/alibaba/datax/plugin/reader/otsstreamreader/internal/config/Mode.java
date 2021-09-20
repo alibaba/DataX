8a705c52c4ed1801d822c8b240b486850dbf8058
removedporn <86824510+removedporn@users.noreply.github.com>
2021-09-21 00:33:08 +0800

    Delete otsstreamreader directory

--- otsstreamreader/src/main/java/com/alibaba/datax/plugin/reader/otsstreamreader/internal/config/Mode.java
+++
@@ -1,8 +0,0 @@
-package com.alibaba.datax.plugin.reader.otsstreamreader.internal.config;
-
-public enum Mode {
-
-    MULTI_VERSION,
-
-    SINGLE_VERSION_AND_UPDATE_ONLY
-}
--- otsstreamreader/src/main/java/com/alibaba/datax/plugin/reader/otsstreamreader/internal/core/IStreamRecordSender.java
+++
@@ -1,9 +0,0 @@
-package com.alibaba.datax.plugin.reader.otsstreamreader.internal.core;
-
-import com.alicloud.openservices.tablestore.model.StreamRecord;
-
-public interface IStreamRecordSender {
-
-    void sendToDatax(StreamRecord streamRecord);
-
-}
