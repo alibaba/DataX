a37693c73b3c8c33665612566e5db3711fd2f98b
removedporn <86824510+removedporn@users.noreply.github.com>
2021-09-21 01:12:24 +0800

    Delete txtfilewriter directory

--- txtfilewriter/src/main/java/com/alibaba/datax/plugin/writer/txtfilewriter/Key.java
+++
@@ -1,9 +0,0 @@
-package com.alibaba.datax.plugin.writer.txtfilewriter;
-
-/**
- * Created by haiwei.luo on 14-9-17.
- */
-public class Key {
-    // must have
-    public static final String PATH = "path";
-}
