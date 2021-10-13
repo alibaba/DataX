9121b7d3c59b0c079aabe69596ad9b01b9f3ed66
zyyang <zyyang@taosdata.com>
2021-10-13 17:12:02 +0800

    change test cases

--- core/src/test/java/com/alibaba/datax/core/EngineTest.java
+++ core/src/test/java/com/alibaba/datax/core/EngineTest.java
@@ -8,9 +8,8 @@ public class EngineTest {
     @Test
     public void test() {
         System.out.println(System.getProperty("java.library.path"));
-//        String[] params = {"-mode", "standalone", "-jobid", "-1", "-job", "/Users/yangzy/workspace/DataX/job/opentsdb2stream.json"};
-        String[] params = {"-mode", "standalone", "-jobid", "-1", "-job", "/Users/yangzy/workspace/DataX/job/opentsdb2tdengine.json"};
-        System.setProperty("datax.home", "/Users/yangzy/workspace/DataX/target/datax/datax");
+        String[] params = {"-mode", "standalone", "-jobid", "-1", "-job", "src/main/job/opentsdb2tdengine.json"};
+        System.setProperty("datax.home", "../target/datax/datax");
         try {
             Engine.entry(params);
         } catch (Throwable e) {
