a3aa8e31eb72f19c15f8f2e419a19eadbc4c73af
hffariel <farielclaire@gmail.com>
2021-10-14 21:48:44 +0800

    optimize columns with backquotes

--- starrockswriter/src/main/java/com/starrocks/connector/datax/plugin/writer/starrockswriter/manager/StarRocksStreamLoadVisitor.java
+++ starrockswriter/src/main/java/com/starrocks/connector/datax/plugin/writer/starrockswriter/manager/StarRocksStreamLoadVisitor.java
@@ -26,6 +26,7 @@ import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
  
 public class StarRocksStreamLoadVisitor {
@@ -137,7 +138,7 @@ public class StarRocksStreamLoadVisitor {
             HttpPut httpPut = new HttpPut(loadUrl);
             List<String> cols = writerOptions.getColumns();
             if (null != cols && !cols.isEmpty()) {
-                httpPut.setHeader("columns", String.join(",", cols));
+                httpPut.setHeader("columns", String.join(",", cols.stream().map(f -> String.format("`%s`", f)).collect(Collectors.toList())));
             }
             if (null != writerOptions.getLoadProps()) {
                 for (Map.Entry<String, Object> entry : writerOptions.getLoadProps().entrySet()) {
