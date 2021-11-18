e4bcc0e50e045711ff1dab3f423bfa4030244ed7
云星 <yunxing.zyj@alibaba-inc.com>
2021-11-18 16:55:02 +0800

    feat: tsdb2tsdb [mput/username/password/database]

--- tsdbreader/src/main/java/com/alibaba/datax/plugin/reader/tsdbreader/conn/Connection4TSDB.java
+++ tsdbreader/src/main/java/com/alibaba/datax/plugin/reader/tsdbreader/conn/Connection4TSDB.java
@@ -23,6 +23,20 @@ public interface Connection4TSDB {
     String address();
 
     /**
+     * Get the address of Database.
+     *
+     * @return host+ip
+     */
+    String username();
+
+    /**
+     * Get the address of Database.
+     *
+     * @return host+ip
+     */
+    String password();
+
+    /**
      * Get the version of Database.
      *
      * @return version
@@ -46,22 +60,27 @@ public interface Connection4TSDB {
     /**
      * Send data points for TSDB with single field.
      */
-    void sendDPs(String metric, Map<String, String> tags, Long start, Long end, RecordSender recordSender) throws Exception;
+    void sendDPs(String metric, Map<String, String> tags, Long start, Long end, RecordSender recordSender, Map<String, Object> hint) throws Exception;
 
     /**
      * Send data points for TSDB with multi fields.
      */
-    void sendDPs(String metric, List<String> fields, Map<String, String> tags, Long start, Long end, RecordSender recordSender) throws Exception;
+    void sendDPs(String metric, List<String> fields, Map<String, String> tags, Long start, Long end, RecordSender recordSender, Map<String, Object> hint) throws Exception;
 
     /**
      * Send data points for RDB with single field.
      */
-    void sendRecords(String metric, Map<String, String> tags, Long start, Long end, List<String> columns4RDB, RecordSender recordSender) throws Exception;
+    void sendRecords(String metric, Map<String, String> tags, Long start, Long end, List<String> columns4RDB, Boolean isCombine, RecordSender recordSender, Map<String, Object> hint) throws Exception;
 
     /**
      * Send data points for RDB with multi fields.
      */
-    void sendRecords(String metric, List<String> fields, Map<String, String> tags, Long start, Long end, List<String> columns4RDB, RecordSender recordSender) throws Exception;
+    void sendRecords(String metric, List<String> fields, Map<String, String> tags, Long start, Long end, List<String> columns4RDB, RecordSender recordSender, Map<String, Object> hint) throws Exception;
+
+    /**
+     * Send data points for RDB with single fields on combine mode.
+     */
+    void sendRecords(List<String> metrics, Map<String, String> tags, Long start, Long end, List<String> columns4RDB, RecordSender recordSender, Map<String, Object> hint) throws Exception;
 
     /**
      * Put data point.
