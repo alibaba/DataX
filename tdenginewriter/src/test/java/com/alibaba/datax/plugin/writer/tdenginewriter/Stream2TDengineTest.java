788cca5ab95d485881c883114f9021887b24229f
zyyang <zyyang@taosdata.com>
2022-02-21 13:43:57 +0800

    change

--- tdenginewriter/src/test/java/com/alibaba/datax/plugin/writer/tdenginewriter/Stream2TDengineTest.java
+++ tdenginewriter/src/test/java/com/alibaba/datax/plugin/writer/tdenginewriter/Stream2TDengineTest.java
@@ -12,7 +12,6 @@ import java.sql.Statement;
 public class Stream2TDengineTest {
 
     private String host2 = "192.168.56.105";
-    private String precision;
 
     @Test
     public void s2t_case1() throws Throwable {
@@ -56,7 +55,8 @@ public class Stream2TDengineTest {
             stmt.execute("drop database if exists db2");
             stmt.execute("create database if not exists db2 precision '" + precision + "'");
             stmt.execute("create table db2.stb2(ts1 timestamp, ts2 timestamp,ts3 timestamp,ts4 timestamp,ts5 timestamp," +
-                    "ts6 timestamp,ts7 timestamp, f1 tinyint, f2 smallint, f3 int, f4 bigint, f5 float, f6 double," +
+                    "ts6 timestamp,ts7 timestamp, ts8 timestamp, ts9 timestamp, ts10 timestamp, f1 tinyint, f2 smallint," +
+                    "f3 int, f4 bigint, f5 float, f6 double," +
                     "f7 bool, f8 binary(100), f9 nchar(100)) tags(t1 timestamp,t2 timestamp,t3 timestamp,t4 timestamp," +
                     "t5 timestamp,t6 timestamp,t7 timestamp, t8 tinyint, t9 smallint, t10 int, t11 bigint, t12 float," +
                     "t13 double, t14 bool, t15 binary(100), t16 nchar(100))");
