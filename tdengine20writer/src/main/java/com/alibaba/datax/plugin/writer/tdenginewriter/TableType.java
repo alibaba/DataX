c4910206f176ddb33eab5b0644193b2063e6962e
lispQin <qinchong@gridsum.com>
2023-01-17 09:10:15 +0800

    ref: modify package name

--- tdengine20writer/src/main/java/com/alibaba/datax/plugin/writer/tdenginewriter/TableType.java
+++
@@ -1,5 +0,0 @@
-package com.alibaba.datax.plugin.writer.tdenginewriter;
-
-public enum TableType {
-    SUP_TABLE, SUB_TABLE, NML_TABLE
-}
---
+++ tdengine30writer/src/main/java/com/alibaba/datax/plugin/writer/tdengine20writer/TableType.java
@@ -0,0 +1,5 @@
+package com.alibaba.datax.plugin.writer.tdengine20writer;
+
+public enum TableType {
+    SUP_TABLE, SUB_TABLE, NML_TABLE
+}
