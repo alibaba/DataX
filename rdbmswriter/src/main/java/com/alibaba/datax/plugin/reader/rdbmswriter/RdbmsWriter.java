34a69a3ec6674363c908117828f0f05dcc8c08fb
luoj <1421128167@qq.com>
2024-12-09 17:08:26 +0800

    xugu replace mode

--- rdbmswriter/src/main/java/com/alibaba/datax/plugin/reader/rdbmswriter/RdbmsWriter.java
+++ rdbmswriter/src/main/java/com/alibaba/datax/plugin/reader/rdbmswriter/RdbmsWriter.java
@@ -28,14 +28,14 @@ public class RdbmsWriter extends Writer {
 
             // warn：not like mysql, only support insert mode, don't use
             String writeMode = this.originalConfig.getString(Key.WRITE_MODE);
-            if (null != writeMode) {
+          /*  if (null != writeMode) {
                 throw DataXException
                         .asDataXException(
                                 DBUtilErrorCode.CONF_ERROR,
                                 String.format(
                                         "写入模式(writeMode)配置有误. 因为不支持配置参数项 writeMode: %s, 仅使用insert sql 插入数据. 请检查您的配置并作出修改.",
                                         writeMode));
-            }
+            }*/
 
             this.commonRdbmsWriterMaster = new SubCommonRdbmsWriter.Job(
                     DATABASE_TYPE);
